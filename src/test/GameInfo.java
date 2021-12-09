6a052d166708d25ccf73f0c529e69c288972fea9
TAN JIAN XIN <marcustjx07@gmail.com>
2021-12-09 20:41:23 +0800

    Add instructions to the GameInfo class

--- src/test/GameInfo.java
+++ src/test/GameInfo.java
@@ -2,6 +2,7 @@ package test;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -32,13 +33,37 @@ public class GameInfo extends TextFrame {
 
     @Override
     public void content() {
-        try {
+        /*try {
             img = ImageIO.read(getClass().getResourceAsStream("InfoBrick.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
         frame.setContentPane(new JLabel(new ImageIcon(img)));
-        frame.add(btn);
+        frame.add(btn);*/
+        String instruction = "<html>" +
+                "<p>Normal Game</p>" +
+                "<ol type = 1>" +
+                "<li> Press A or D to move left or right.</li>" +
+                "<li> There are 4 levels in total.</li>" +
+                "<li> Win = Break all the bricks.</li>" +
+                "<li> Lose = No balls left.</li>" +
+                "<li> Score is recorded after each level.</li>" +
+                "</ol>" +
+                "<p>Special Game</p>" +
+                "<ol type = 1>" +
+                "<li>Press A or D to move left or right.</li>" +
+                "<li>There is only one level.</li>" +
+                "<li>90 seconds gameplay.</li>" +
+                "<li>Win = Survive for 90 seconds or break all the bricks.</li>" +
+                "<li>Lose = No balls left.</li>" +
+                "<li>Score is recorded after gameplay.</li>" +
+                "</ol>" +
+                "</html>";
+        JLabel label = new JLabel(instruction);
+        label.setBounds(10, 60, 511, 380);
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("serif", Font.BOLD, 18));
+        frame.add(label);
         frame.setVisible(true);
     }
 }
