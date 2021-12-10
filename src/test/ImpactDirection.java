7d62f04bccf534fbf5e7b5def435b25208970b8c
TAN JIAN XIN <marcustjx07@gmail.com>
2021-12-10 11:20:33 +0800

    Add enum to get rid of conditional statement (switch statement)

---
+++ src/test/ImpactDirection.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.awt.*;
+
+public enum ImpactDirection {
+
+    UP_IMPACT {
+        @Override
+        public boolean setImpact(Brick brick, Ball ball, boolean isRubber) {
+            if(isRubber)
+                ball.reverseY();
+            return brick.setImpact(ball.down, Crack.UP);
+        }
+    },
+    DOWN_IMPACT{
+        @Override
+        public boolean setImpact(Brick brick, Ball ball, boolean isRubber) {
+            if(isRubber)
+                ball.reverseY();
+            return brick.setImpact(ball.down, Crack.UP);
+        }
+    },
+    LEFT_IMPACT {
+        @Override
+        public boolean setImpact(Brick brick, Ball ball, boolean isRubber) {
+            if(isRubber)
+                ball.reverseX();
+            return brick.setImpact(ball.right, Crack.RIGHT);
+        }
+    },
+    RIGHT_IMPACT {
+        @Override
+        public boolean setImpact(Brick brick, Ball ball, boolean isRubber) {
+            if(isRubber)
+                ball.reverseX();
+            return brick.setImpact(ball.left, Crack.LEFT);
+        }
+    };
+
+    public abstract boolean setImpact(Brick brick, Ball ball, boolean isRubber);
+}
